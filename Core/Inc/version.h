a963a410df842a506c174c554b1c2bc5945cba69
djalex95 <a_haeckelmoser@yahoo.de>
2026-07-07 22:22:33 +0200

    CI: Release nur per Tag; FW 1.2.3-dev

--- Core/Inc/version.h
+++ Core/Inc/version.h
@@ -8,7 +8,7 @@
 #define INC_VERSION_H_
 
 /* Firmware-Version der Applikation. Bei jeder Freigabe erhöhen. */
-#define FW_VERSION       "1.2.2"
+#define FW_VERSION       "1.2.3-dev"
 #define FW_VERSION_DATE  "2026-07-07"
 
 /* Kombinierte Anzeige, z. B. "1.2.0 (2026-07-07)". */
